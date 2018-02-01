ce9d4720a12a958d2b0c6a9f190561eab8c09670
tanyakino <34406475+tanyakino@users.noreply.github.com>
2018-02-01 03:22:03 -0800

    Loop work

--- LoopPrac3.c
+++ LoopPrac3.c
@@ -1,3 +1,14 @@
+/*
+
+Enter the number: 5
+........1
+.......22
+......333
+.....4444
+....55555
+
+*/
+
 #include <stdio.h>
 
 int main ()
@@ -6,16 +17,29 @@ int main ()
 
 	printf ("Enter the number: ");
 	scanf ("%d", &n);
-
+ 	
+ 	// pushing the loop to print numbers
+	// if i = 1 then it'll work one by one 
 	for (int i = 1; i <= n; ++i)
 	{	
-		for (int j = n; j <= n; --j)
+		// print docs max to min
+		for (int j = n; i < j; --j) 
 		{
-			printf (".");
+			printf (".");	
 		}
 
-		printf ("%d\n", i);
+		printf ("%d", i); // print first 1 that is increased ev.time
+ 
+ 		// print the numbers after dots
+		// k = 2 ev.time this loop reenters
+		for (int k = 2; k <= i; k++)
+		{
+			printf("%d", i);
+		}
+
+		printf ("\n");
 	}	
+		
 
 	return 0;
 }
