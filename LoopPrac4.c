c6b792671d82e3be5778725f33f6037050c33980
tanyakino <34406475+tanyakino@users.noreply.github.com>
2018-02-03 23:49:14 -0800

    loops alm done

--- LoopPrac4.c
+++ LoopPrac4.c
@@ -2,19 +2,30 @@
 
 int main ()
 {
-	int w, h;
+	int rows, columns;
 
-	printf ("Enter width: ");
-	scanf ("%d", &w);
-	printf ("Enter height: ");
-	scanf ("%d", &h);
+	printf ("Enter rows: ");
+	scanf ("%d", &rows);
+	printf ("Enter columns: ");
+	scanf ("%d", &columns);
 
-	for(int i = 1; i <= w; ++i)
+	//отвечает за то, сколько раз мы пойдем вниз
+	for(int i = 1; i <= rows; ++i) 
+	{
+		printf ("\n");
+
+		//отвечает за * сверху вниз в колонне
+		for (int j = 1; j <= columns; ++j)
 		{
+			if (i % 2 == 1)
 			printf ("* ");
+
+			//if (((i * j) % 2) ==  0)
+				//printf ("o ");
+			//if ( i == j || i % 2 == 0)
+				//printf ("o ");
 		}
-
-		for ()
-
+	}
+		
 	return 0;
 }
